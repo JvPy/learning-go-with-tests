6942efc7958006a2a21cb605c09a9017aabe9058
João Victor Martinelli de Souza <joaovictor.martinelli@pca.com.br>
2020-04-02 21:49:53 -0300

    new features to Hello,world with tests

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,14 +3,31 @@ package main
 import "fmt"
 
 const englishHello = "Hello, "
+const spanishHello = "Hola, "
+const portugueseHello = "Olá, "
 
 func main() {
-	fmt.Println(SayHello("Joao"))
+	fmt.Println(SayHello("Joao", "Portuguese"))
 }
 
-func SayHello(name string) string {
+func SayHello(name string, lang string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHello + name
+
+	return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) (prefix string) {
+
+	switch lang {
+	case "Portuguese":
+		prefix = portugueseHello
+	case "Spanish":
+		prefix = spanishHello
+	default:
+		prefix = englishHello
+	}
+
+	return
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -12,16 +12,28 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Say hello to user", func(t *testing.T) {
-		got := SayHello("Joao")
+		got := SayHello("Joao", "")
 		want := "Hello, Joao"
 
 		ErrorMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, world' when empty string", func(t *testing.T) {
-		got := SayHello("")
+		got := SayHello("", "")
 		want := "Hello, world"
 
+		ErrorMessage(t, got, want)
+	})
+
+	t.Run("Say hello to spanish user", func(t *testing.T) {
+		got := SayHello("Arturo", "Spanish")
+		want := "Hola, Arturo"
+		ErrorMessage(t, got, want)
+	})
+
+	t.Run("Say hello to portuguese user", func(t *testing.T) {
+		got := SayHello("Joao", "Portuguese")
+		want := "Olá, Joao"
 		ErrorMessage(t, got, want)
 	})
 }
